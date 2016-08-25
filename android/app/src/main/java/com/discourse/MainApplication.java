c963ea7678119e206a724218f4cf2ba1d330fc41
Joffrey Jaffeux <j.jaffeux@gmail.com>
2016-08-25 21:17:13 +0200

    s/DiscourseMobile/Discourse

--- android/app/src/main/java/com/discourse/MainApplication.java
+++ android/app/src/main/java/com/discourse/MainApplication.java
@@ -8,8 +8,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.bitgo.randombytes.RandomBytesPackage;
-
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,8 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new RandomBytesPackage()
+          new MainReactPackage()
       );
     }
   };
