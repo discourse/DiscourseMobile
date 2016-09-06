41e258063dc32a22c8007347843442bb63715426
Sam Saffron <sam.saffron@gmail.com>
2016-09-06 22:48:36 +1000

    start getting a browser in place for android

---
+++ js/components/Browser.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import { WebView } from 'react-native'
+
+class Browser extends React.Component {
+
+  render() {
+    <WebView source={{uri: this.state.url}} />
+  }
+
+}
+
+export default Discourse
