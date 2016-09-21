3a467f87d16521fad65edffa663bee5212918762
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2016-09-22 01:03:07 +0200

    adds a background to the ProgressBar

--- js/ProgressBar.js
+++ js/ProgressBar.js
@@ -2,6 +2,9 @@
 'use strict'
 
 import React from 'react'
+
+import { View } from 'react-native'
+
 import Dimensions from 'Dimensions'
 
 import { Bar } from 'react-native-progress'
@@ -10,17 +13,30 @@ import colors from './colors'
 
 class ProgressBar extends React.Component {
   render() {
+    let height = this.props.progress === 0 ? 0 : 3
+
     return (
-      <Bar
-        color={colors.blueCallToAction}
-        borderWidth={0}
-        borderRadius={0}
-        style={{position:'absolute', bottom: 0, left: 0, zIndex: 10}}
-        height={this.props.progress === 0 ? 0 : 3}
-        progress={this.props.progress}
-        width={Dimensions.get('window').width}
-      />
+      <View style={[styles.container, {height: height}]}>
+        <Bar
+          color={colors.blueCallToAction}
+          borderWidth={0}
+          borderRadius={0}
+          height={height}
+          progress={this.props.progress}
+          width={Dimensions.get('window').width}
+        />
+      </View>
     )
+  }
+}
+
+const styles = {
+  container: {
+    zIndex: 10,
+    position:'absolute',
+    bottom: 0,
+    left: 0,
+    backgroundColor: colors.grayBackground
   }
 }
 
