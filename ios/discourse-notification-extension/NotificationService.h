f3e7868687464774a213a8b47a934df147f668de
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-09-21 10:45:43 +0200

    init app extension

---
+++ ios/discourse-notification-extension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  discourse-notification-extension
+//
+//  Created by Joffrey Jaffeux on 21/09/2018.
+//  Copyright © 2018 Discourse. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
