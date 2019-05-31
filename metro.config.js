e9b4dc6662a13c221d6678e9628a895b8fd1ba43
Penar Musaraj <pmusaraj@gmail.com>
2019-05-31 09:29:18 -0400

    Add loading screen to webview
    - uses a fork of react-native-webview temporarily to fix a UI bug when using a loading screen, PR pending

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,7 @@
  *
  * @format
  */
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
   transformer: {
@@ -14,4 +15,9 @@ module.exports = {
       },
     }),
   },
+	resolver: {
+	    blacklistRE: blacklist([
+	        /node_modules\/.*\/node_modules\/react-native\/.*/,
+	    ])
+	},
 };
