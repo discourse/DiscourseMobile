ad20cd4b931075961baf458f1ac8485b41333d6e
Jarek Radosz <jradosz@gmail.com>
2022-09-23 01:15:09 +0200

    DEV: Convert OnboardingView to a function
    And slightly rework the view tree (to remove `{'\n'}`)

--- js/screens/HomeScreenComponents/OnBoardingView.js
+++ js/screens/HomeScreenComponents/OnBoardingView.js
@@ -1,79 +1,62 @@
 /* @flow */
 'use strict';
 
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 import i18n from 'i18n-js';
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {ThemeContext} from '../../ThemeContext';
 
-class OnBoardingView extends React.Component {
-  static propTypes = {
-    onDidPressAddSite: PropTypes.func.isRequired,
-  };
+const OnBoardingView = props => {
+  const theme = useContext(ThemeContext);
+  const img =
+    theme.name === 'light'
+      ? require('../../../img/onboarding.png')
+      : require('../../../img/onboarding-dark.png');
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const theme = this.context;
-    const img =
-      theme.name === 'light'
-        ? require('../../../img/onboarding.png')
-        : require('../../../img/onboarding-dark.png');
-    return (
-      <View style={{backgroundColor: theme.grayBackground, flex: 1}}>
-        <View style={styles.illustrationContainer}>
-          <Image
-            style={{width: '100%', height: '100%'}}
-            source={img}
-            resizeMode="contain"
-          />
-        </View>
-        <View style={styles.addSiteContainer}>
-          <Text style={styles.text}>
-            <Text style={{...styles.title, color: theme.grayTitle}}>
-              {i18n.t('no_sites_yet')}
-            </Text>
-            {'\n'}
-            <Text style={{color: theme.graySubtitle}}>
-              {i18n.t('add_sites')}
-            </Text>
-          </Text>
-
-          <TouchableOpacity onPress={() => this.props.onDidPressAddSite()}>
-            <Text
-              style={{
-                ...styles.addSiteButtonText,
-                backgroundColor: theme.blueCallToAction,
-                color: theme.buttonTextColor,
-              }}>
-              {i18n.t('add_first_site')}
-            </Text>
-          </TouchableOpacity>
-        </View>
+  return (
+    <View style={{backgroundColor: theme.grayBackground, flex: 1}}>
+      <View style={styles.illustrationContainer}>
+        <Image
+          style={{width: '100%', height: '100%'}}
+          source={img}
+          resizeMode="contain"
+        />
       </View>
-    );
-  }
-}
+      <View style={styles.addSiteContainer}>
+        <View style={styles.text}>
+          <Text style={{...styles.title, color: theme.grayTitle}}>
+            {i18n.t('no_sites_yet')}
+          </Text>
+          <Text style={{...styles.subtitle, color: theme.graySubtitle}}>
+            {i18n.t('add_sites')}
+          </Text>
+        </View>
 
-OnBoardingView.contextType = ThemeContext;
+        <TouchableOpacity onPress={() => props.onDidPressAddSite()}>
+          <Text
+            style={{
+              ...styles.addSiteButtonText,
+              backgroundColor: theme.blueCallToAction,
+              color: theme.buttonTextColor,
+            }}>
+            {i18n.t('add_first_site')}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+OnBoardingView.propTypes = {
+  onDidPressAddSite: PropTypes.func.isRequired,
+};
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    padding: 32,
-    textAlign: 'center',
-  },
-  addSiteButtonText: {
-    fontSize: 20,
-    fontWeight: '500',
-    padding: 16,
-    textAlign: 'center',
-  },
-  title: {
-    fontWeight: '500',
+  illustrationContainer: {
+    marginTop: 100,
+    height: '40%',
+    width: '100%',
   },
   addSiteContainer: {
     marginTop: 32,
@@ -81,10 +64,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'column',
   },
-  illustrationContainer: {
-    marginTop: 100,
-    height: '40%',
-    width: '100%',
+  text: {
+    alignItems: 'center',
+    padding: 32,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  subtitle: {
+    fontSize: 18,
+  },
+  addSiteButtonText: {
+    fontSize: 20,
+    fontWeight: '500',
+    padding: 16,
+    textAlign: 'center',
   },
 });
 
