398a04913b2ac670645126c9ce5ce4c3338b0aac
Penar Musaraj <pmusaraj@gmail.com>
2024-10-15 22:19:16 -0400

    Fix share extension regression

--- ios/ShareExtension/ShareViewController.swift
+++ ios/ShareExtension/ShareViewController.swift
@@ -3,7 +3,6 @@
 //  ShareExtension
 //
 //  Created by Penar Musaraj on 2020-02-13.
-//  Copyright © 2020 Facebook. All rights reserved.
 //
 
 import UIKit
@@ -46,7 +45,9 @@ class ShareViewController: UIViewController {
                   }
 
                   DispatchQueue.main.async {
-                    self.openURL(URL(string: "discourse://share?sharedUrl=\(url)")!)
+                    if let application = UIApplication.value(forKeyPath: #keyPath(UIApplication.shared)) as? UIApplication {
+                        application.open(URL(string: "discourse://share?sharedUrl=\(url)")!, options: [:], completionHandler: nil)
+                    }
                   }
               }
           } else if attachment.isText {
@@ -60,7 +61,9 @@ class ShareViewController: UIViewController {
                   }
 
                   DispatchQueue.main.async {
-                    self.openURL(URL(string: "discourse://share?sharedUrl=\(url)")!)
+                    if let application = UIApplication.value(forKeyPath: #keyPath(UIApplication.shared)) as? UIApplication {
+                        application.open(URL(string: "discourse://share?sharedUrl=\(url)")!, options: [:], completionHandler: nil)
+                    }
                   }
               }
           }
@@ -74,16 +77,4 @@ class ShareViewController: UIViewController {
     })
   }
 
-  func openURL(_ url: URL) {
-    let selector = sel_registerName("openURL:")
-    var responder: UIResponder? = self
-
-    while responder != nil {
-        if let application = responder as? UIApplication {
-          application.perform(selector, with: url)
-          break
-        }
-        responder = responder?.next
-    }
-  }
 }
